9bc0185c376872f20246e6e81192a2be3179adc3
opilane <opilane@HP-CZC2349HTQ>
2025-12-11 11:20:58 +0200

    parool

--- PythonApplication4/MyModule.py
+++ PythonApplication4/MyModule.py
@@ -1,1 +1,37 @@
+#Registreerimine
 
+k=[]
+s=[]
+
+def registreerimine(k:list, s:list)->any:
+    kasutajanimi=input("Sisesta kasutajanimi: ")
+    if kasutajanimi in k:
+        return
+
+    parool=input("Sisesta parool: ")
+
+    k.append(kasutajanimi)
+    s.append(parool)
+
+def autoriseerimine(k:list, s:list)->any:
+    kasutajanimi=input("Sisesta kasutajanimi: ")
+    if kasutajanimi not in k:
+        return
+    parool = input("Sisesta parool: ")
+
+    i=k.index(kasutajanimi)
+    if s[i]==parool:
+        print("Sisselogitud!")
+    else:
+        print("Vale parool!")
+
+def paroolivahetus(k:list, s:list)->any:
+    vastus=input("Kas te tahate muuta parool? (Jah/Ei): ")
+    if vastus=='Jah':
+
+        print("Sisesta uus parool: ")
+    else:
+        print("Head aega")
+
+def paroolitaasramine(k:list, s:list)->any:
+    pass
--- PythonApplication4/Süsteem_põhifail.py
+++ PythonApplication4/Süsteem_põhifail.py
@@ -1,1 +1,27 @@
+from MyModule import *
 
+k=['Mari', 'Juku', 'Kati']
+s=['1234', '5678', 'abcd']
+
+while True:
+    print("Vali tegevus: ")
+    print("1- registreerimine")
+    print("2- autoriseerimine")
+    print("3- paroolivahetus")
+    print("4- paroolitaastamine")
+    print("5- välju")
+
+    valik=input("Sisesta valik (1-5): ")
+    if valik=='1':
+        registreerimine(k, s)
+    elif valik=='2':
+        autoriseerimine(k, s)
+    elif valik=='3':
+        paroolivahetus(k, s)
+    elif valik=='4':
+        paroolitaasramine(k, s)
+    elif valik=='5':
+        print("Väljun programmist.")
+        break
+    else:
+        print("Vigane valik, proovi uuesti.")
